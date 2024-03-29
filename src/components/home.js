4c7fb1ce57961746f4f899db471a96a4cfeff28d
SriRamaPandian <sriramapandian147@gmail.com>
2024-03-29 16:59:03 +0530

    some_changes

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import navebar from "./navebar"
+
+const home = () => {
+  return (
+    <div className="home-container">
+        <navebar />
+    </div>
+  )
+}
+
+export default home
---
+++ src/components/navebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from "../assets/Logo.svg"
+
+const navebar = () => {
+  return (
+    <div>navebar</div>
+  )
+}
+
+export default navebar
